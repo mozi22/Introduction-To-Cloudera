fb783910afd3cf5c55ac8852fc8fd860b474738c
mozi22 <muazzamalii@hotmail.com>
2016-10-25 15:13:49 +0200

    First commit

---
+++ src/main/java/msp_exercise/exercise/App.java
@@ -0,0 +1,13 @@
+package msp_exercise.exercise;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
